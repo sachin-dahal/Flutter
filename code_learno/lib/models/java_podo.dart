088daab355216db773143659b817ec8dc57dd451
sachin-dahal <56621935+sachin-dahal@users.noreply.github.com>
2020-07-22 08:44:01 +0545

    model update

---
+++ code_learno/lib/models/java_podo.dart
@@ -0,0 +1,8 @@
+class Java {
+  final String heading;
+  final String imageUrl;
+  final String body;
+  final String conclusion;
+
+  Java({this.heading, this.body, this.imageUrl, this.conclusion});
+}
---
+++ code_learno/lib/models/python_podo.dart
@@ -0,0 +1,8 @@
+class Python {
+  final String heading;
+  final String body;
+  final String imageUrl;
+  final String conclusion;
+
+  Python({this.heading, this.body, this.imageUrl, this.conclusion});
+}
