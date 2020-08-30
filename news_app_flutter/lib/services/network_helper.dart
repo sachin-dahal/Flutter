cc89ec4ad8a23112c0ebe4f8bc24e97ad7a099b7
sachin-dahal <56621935+sachin-dahal@users.noreply.github.com>
2020-08-30 20:46:40 +0545

    api update

--- news_app_flutter/lib/services/network_helper.dart
+++ news_app_flutter/lib/services/network_helper.dart
@@ -7,9 +7,9 @@ class NetworkHelper {
   List<NewsDataModel> news = [];
 
   Future fetchData(String about) async {
-    final String apiKey = "dcf3a9add9174bbe8237af08c1d262ab";
+    final String apiKey = "df11f8afddfa4e888f5378d6e2bf9da9";
     final String urlNews =
-        "http://newsapi.org/v2/everything?q=$about&from=2020-07-27&sortBy=publishedAt&apiKey=$apiKey&fbclid=IwAR0RaIxXKDa-eSmTdQrLAXHDbJEPSBNsA9TPGy0RjIWGf0ZrM01tRa-pHTc";
+        "http://newsapi.org/v2/everything?q=$about&from=2020-07-30&sortBy=publishedAt&apiKey=$apiKey&fbclid=IwAR0RaIxXKDa-eSmTdQrLAXHDbJEPSBNsA9TPGy0RjIWGf0ZrM01tRa-pHTc";
 
     final response = await get(urlNews);
     if (response.statusCode == 200) {
