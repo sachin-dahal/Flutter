783226142165ec4b1e58b4c97709d5d2b1a68d0c
sachin-dahal <56621935+sachin-dahal@users.noreply.github.com>
2020-09-19 20:25:19 +0545

    Update news_data_model.dart

--- news_app_flutter/lib/model/news_data_model.dart
+++ news_app_flutter/lib/model/news_data_model.dart
@@ -6,23 +6,23 @@ class NewsDataModel {
   final String newsContent;
   final String newsImageUrl;
 
-NewsDataModel {
-	this.newsAuthor,
-	this.publishedDate,
-	this.newsTitle,
-	this.newsDescription,
-	this.newsContent,
-	this.newsImageUrl,
-}
+  NewsDataModel({
+    this.newsAuthor,
+    this.publishedDate,
+    this.newsTitle,
+    this.newsDescription,
+    this.newsContent,
+    this.newsImageUrl,
+  });
 
-  factory NewsDataModel.fromJson(Map<String, dynamic> parsedData){
-	return NewsDataModel(
-		newsAuthor : parsedData['author'],
-        	publishedDate : parsedData['publishedAt'],
-        	newsTitle : parsedData['title'],
-        	newsDescription : parsedData['description'],
-        	newsContent : parsedData['content'],
-        	newsImageUrl : parsedData['urlToImage'],
-	);
-		}
+  factory NewsDataModel.fromJson(Map<String, dynamic> parsedData) {
+    return NewsDataModel(
+      newsAuthor: parsedData['author'],
+      publishedDate: parsedData['publishedAt'],
+      newsTitle: parsedData['title'],
+      newsDescription: parsedData['description'],
+      newsContent: parsedData['content'],
+      newsImageUrl: parsedData['urlToImage'],
+    );
+  }
 }
